f4c6a1802678f817d8be914c501f6345f97f4c87
한건우 <gunwoo9643@naver.com>
2025-08-14 11:40:05 +0900

    삽입, 힙 정렬
    정렬

---
+++ heap_sort.py
@@ -0,0 +1,30 @@
+import sys
+
+def heapify(arr, n, i):
+    largest = i
+    l = 2*i + 1
+    r = 2*i + 2
+    if l < n and arr[l] > arr[largest]:
+        largest = l
+    if r < n and arr[r] > arr[largest]:
+        largest = r
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+
+def heap_sort(arr):
+    n = len(arr)
+    for i in range(n//2 - 1, -1, -1):
+        heapify(arr, n, i)
+    for i in range(n-1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 heap_sort.py data.txt")
+        sys.exit(1)
+    with open(sys.argv[1]) as f:
+        data = list(map(int, f.read().split()))[:100]
+    heap_sort(data)
+    print(data)
